4332a4f406d8d2ba53a3e589d7ed1e82898b5d46
www-data <www-data@vps1767.first-root.com>
2016-03-21 11:25:34 -0300

    Wiki media cap1:secoes:rec_tercos.jpg uploaded by Humberto Bortolossi

--- bin/dw2tex.lua
+++ bin/dw2tex.lua
@@ -40,7 +40,7 @@ local killunknown = Cs( ( C(known) / '%1' + C(P(1)) / '(símbolo desconhecido)' 
 doc = killunknown:match(doc)
 
 local special = P('**') + P('__') + P([[//]]) + P("''") + P('====') + P('$') + P('<WRAP')
-   + P('</WRAP') + P('"') + P([[\\]]) + P('{{') + P('}}')
+   + P('</WRAP') + P('"') + P([[\\]]) + P('{{') + P('}}') + P('/*') + P('*/')
 local harmless = known - special
 
 local simpletext = harmless^1
@@ -52,9 +52,13 @@ local quote = surround('quote', '"', simpletext)
 local newline = token('newline', [[\\]])
 local simplemath = surround('simplemath', '$', simpletext)
 local title = P('=====') * token('title', simpletext) * P('=====')
+local titlechapter = P('======') * token('title', simpletext) * P('======')
+local titleless = P('====') * token('title', simpletext) * P('====')
+local include = P('{{page>') * token('include', simpletext) * P('}}')
 local image = P('{{') * token('image', simpletext) * P('}}')
-local decotext = bold + under + italic + mono + quote + newline + simplemath + title + image + token('simple', simpletext)
-
+local comment = P('/*') * token('comment', simpletext + bold + under + italic + mono + quote + newline + simplemath + titlechapter + title + titleless)^0 * P('*/')
+local decotext = bold + under + italic + mono + quote + newline + simplemath + titlechapter + title + titleless + include + image
+                 + comment + token('simple', simpletext)
 local W = V'W'
 local envname = P('professor') + P('exercicio') + P('resposta') + P('abstrato') + P('conexoes') + P('explorando') + P('imagem') + P('introdutorio') + P('massa') + P('refletindo') + P('figura') + P('nota')
 local wrap = P{
@@ -70,40 +74,56 @@ local finalsymb = (P('#') / [[\#]]) + (P('$') / [[\$]]) + (P([[%]]) / [[\%%]]) +
 
 local formatimage = Cs( ( P('') / '/var/www/livro/data/gitrepo/media' ) * ( C(alpha + digit + S('-_.'))
                     + ( P(' ') / '' ) + ( P(':') / '/' ) )^1 * ( simpletext / '' ) )
+local formatinclude = Cs( ( P('') / '/var/www/livro/data/gitrepo/pages/' )
+      * ( C(alpha + digit + S('-_.')) + ( P(' ') / '' ) + ( P(':') / '/' ) )^1
+      * ( P('') / '.txt' ) * ( simpletext / '' )^0 )
 local formatsimple = Cs( ((finalsymb) + C(known))^1 )
 
 function texprint (tbl, indent)
   local outstr = ""
   if not indent then indent = 0 end
   for k, v in pairs(tbl) do
-    local formatting = string.rep("  ", indent)
-    if (v.tag) == "title" then
-       outstr = outstr .. formatting .. '\\section{' .. formatsimple:match(v.value) .. '}\n'
-       --tprint(v, indent + 1)
-    elseif (v.tag) == 'bold' then
-       outstr = outstr .. formatting .. '{\\bf ' .. formatsimple:match(v.value) .. '}'
-    elseif (v.tag) == 'italic' then
-       outstr = outstr .. formatting .. '{\\it ' .. formatsimple:match(v.value) .. '}'
-    elseif (v.tag) == 'under' then
-       outstr = outstr .. formatting .. '{' .. formatsimple:match(v.value) .. '}'
-    elseif (v.tag) == 'quote' then
-       outstr = outstr .. formatting .. [[``]] .. formatsimple:match(v.value) .. [['']]
-    elseif (v.tag) == 'newline' then
-       outstr = outstr .. formatting .. '\\newline '
-    elseif (v.tag) == 'simple' then
-       outstr = outstr .. formatting .. formatsimple:match(v.value)
-       --print(formatting .. v.value)
-    elseif (v.tag) == 'image' then
-       outstr = outstr .. formatting .. '\\includegraphics[width=\\textwidth]{' .. formatimage:match(v.value) .. '}\n'
-    elseif (v.tag) == 'error' then
-       outstr = outstr .. formatting .. 'ERRO:\\{' .. formatsimple:match(v.value) .. '\\}'
-    elseif (v.tag) == 'wrap' then
-       outstr = outstr .. formatting .. '\\begin{' .. v.type .. '}{}{}'
-       outstr = outstr .. texprint(v.value, indent + 1)
-       outstr = outstr .. formatting .. '\\end{' .. v.type .. '}'
-    end
-  end
-  return outstr
+     local formatting = string.rep("  ", indent)
+     if (v.tag) == "title" then
+        outstr = outstr .. formatting .. '\\section{' .. formatsimple:match(v.value) .. '}\n'
+     elseif (v.tag) == "titleless" then
+        outstr = outstr .. formatting .. '\\subsection{' .. formatsimple:match(v.value) .. '}\n'
+     elseif (v.tag) == "titlechapter" then
+        outstr = outstr .. formatting .. '\\chapter{' .. formatsimple:match(v.value) .. '}\n'
+     elseif (v.tag) == 'bold' then
+        outstr = outstr .. formatting .. '{\\bf ' .. formatsimple:match(v.value) .. '}'
+     elseif (v.tag) == 'italic' then
+        outstr = outstr .. formatting .. '{\\it ' .. formatsimple:match(v.value) .. '}'
+     elseif (v.tag) == 'under' then
+        outstr = outstr .. formatting .. '{' .. formatsimple:match(v.value) .. '}'
+     elseif (v.tag) == 'quote' then
+        outstr = outstr .. formatting .. [[``]] .. formatsimple:match(v.value) .. [['']]
+     elseif (v.tag) == 'newline' then
+        outstr = outstr .. formatting .. '\\newline '
+     elseif (v.tag) == 'simple' then
+        outstr = outstr .. formatting .. formatsimple:match(v.value)
+        --print(formatting .. v.value)
+     elseif (v.tag) == 'include' then
+        local includefilename = formatinclude:match(v.value)
+        includefile = io.open(includefilename, 'r')
+        if (includefile) then
+           local includestring = includefile:read("*all")
+           includefile:close()
+           outstr = outstr .. formatting .. texprint(document:match(includestring))
+        end
+     elseif (v.tag) == 'image' then
+        outstr = outstr .. formatting .. '\n\n'
+           .. formatting .. '\\includegraphics[width=\\textwidth, height=4cm, keepaspectratio]{'
+           .. formatimage:match(v.value) .. '}\n\n'
+     elseif (v.tag) == 'error' then
+        outstr = outstr .. formatting .. 'ERRO:\\{' .. formatsimple:match(v.value) .. '\\}'
+     elseif (v.tag) == 'wrap' then
+        outstr = outstr .. formatting .. '\\begin{' .. v.type .. '}{}{}'
+        outstr = outstr .. texprint(v.value, indent + 1)
+        outstr = outstr .. formatting .. '\\end{' .. v.type .. '}'
+     end
+   end
+   return outstr
 end
 
 file = io.open ('header.tex', "r")
